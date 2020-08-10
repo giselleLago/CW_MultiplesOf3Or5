72de71f37215e71dc7f7ea5fb736209b8cc81910
giselleLago <gisellelago25994@gmail.com>
2020-08-10 10:24:53 +0200

    Add project files.

---
+++ CW_MultiplesOf3Or5/Kata.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CW_MultiplesOf3Or5
+{
+    public class Kata
+    {
+        public int Solution(int value)
+        {
+            return Enumerable.Range(0, value).Where(e => e % 3 == 0 || e % 5 == 0).Sum();
+
+            /* var count = 0;
+             for (int i = 0; i < value; i++)
+             {
+                 if (i % 3 == 0 || i % 5 == 0)
+                 {
+                     count += i;
+                 }
+             }
+             return count;*/
+        }
+    }
+}
---
+++ CW_MultiplesOf3Or5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CW_MultiplesOf3Or5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var a = new Kata();
+            Console.WriteLine(a.Solution(10));
+            Console.ReadKey();
+        }
+    }
+}
